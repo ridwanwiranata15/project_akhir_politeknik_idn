5fc7c4e1af294f9cf35da6f49cb01455666fe083
ridwanwiranata15 <jagokoding15@gmail.com>
2024-10-28 20:03:30 +0700

    membuat halaman admin dan authentication

--- js/admin.js
+++ js/admin.js
@@ -1,5 +1,17 @@
-let menuicn = document.querySelector('.menuicn')
-let nav = document.querySelector('.navcontainer')
-menuicn.addEventListener('click', ()=>{
-    nav.classList.toggle('navclose')
+const ButtonOpenModalCreate = document.querySelector('.header-button button');
+ButtonOpenModalCreate.addEventListener('click', function(){
+    document.querySelector('.modal-create').style.display = "block";
+    document.querySelector('.modal-create-content-footer button').classList.remove('btn-update-data')
+    document.querySelector('.modal-create-content-footer button').classList.add('btn-save-data')
+    document.querySelector('.modal-create-content-footer button').innerText = "Tambah"
+    document.querySelector('.modal-create-content-header h1').innerText = "Tambah data dokter"
 })
+document.querySelector('.modal-create-content-header span').addEventListener('click', function(){
+    document.querySelector('.modal-create').style.display = "none";
+});
+function edit(id){
+    document.querySelector('.modal-create').style.display = "block";
+    document.querySelector('.modal-create-content-footer button').classList.add('btn-update-data')
+    document.querySelector('.modal-create-content-header h1').innerText = "Ubah data dokter"
+    document.querySelector('.modal-create-content-footer button').innerText = "Ubah"
+}
