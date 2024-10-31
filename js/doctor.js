20f16f3be49d49e641ac7e7a9c2589e73f00c2f7
ridwanwiranata15 <jagokoding15@gmail.com>
2024-10-31 15:48:07 +0700

    mencari data dokter berserta input sebagai fungsi update data - 31 10 2024

--- js/doctor.js
+++ js/doctor.js
@@ -3,4 +3,8 @@ function editDoctor(id){
     document.querySelector('.modal-create-content-footer button').classList.add('btn-update-data')
     document.querySelector('.modal-create-content-header h1').innerText = "Ubah data dokter"
     document.querySelector('.modal-create-content-footer button').innerText = "Ubah"
+
+    const doctors = document.querySelectorAll('tr[data-id="'+id+'"] td');
+    const inputs = document.querySelectorAll('.modal-create-content-body input');
+    console.log(inputs);
 }
